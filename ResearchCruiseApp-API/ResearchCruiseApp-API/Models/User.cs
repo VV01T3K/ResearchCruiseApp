1d5e76480ea9bc8c8b7fc50997944b249a8eaaa7
Mateusz Kowalczyk <mateusz.kowalczyyk@sspg.pl>
2023-12-21 19:18:35 +0100

    Authorization simplified

---
+++ ResearchCruiseApp-API/ResearchCruiseApp-API/Models/User.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace ResearchCruiseApp_API.Models;
+
+public class User : IdentityUser
+{
+    public string FirstName { get; set; } = null!;
+    public string LastName { get; set; } = null!;
+}
