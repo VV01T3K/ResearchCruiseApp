337400baab722b62929d8cda1e4427d03152b138
Mateusz Kowalczyk <mateusz.kowalczyyk@sspg.pl>
2024-01-04 21:13:13 +0100

    Moved IdentityAuthenticationController.cs content to AccountController.cs

--- ResearchCruiseApp-API/ResearchCruiseApp-API/Controllers/AccountController.cs
+++ ResearchCruiseApp-API/ResearchCruiseApp-API/Controllers/AccountController.cs
@@ -1,21 +1,114 @@
 using Microsoft.AspNetCore.Authorization;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.CodeAnalysis.CSharp.Syntax;
+using System.ComponentModel.DataAnnotations;
+using System.Diagnostics;
+using Microsoft.AspNetCore.Authentication.BearerToken;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Identity;
+using ResearchCruiseApp_API.Data;
+using ResearchCruiseApp_API.Models;
 
 namespace ResearchCruiseApp_API.Controllers
 {
-    [Authorize]
     [Route("[controller]")]
     [ApiController]
     public class AccountController : ControllerBase
     {
-        [HttpGet]
-        public IActionResult GetCurrentUser()
+        private readonly EmailAddressAttribute _emailAddressAttribute = new();
+        
+        
+        [HttpPost("register")]
+        public async Task<Results<Ok, ValidationProblem>> Register(
+            [FromBody] UserRegistrationModel userRegistrationModel,
+            [FromServices] IServiceProvider serviceProvider)
         {
-            var user = this.User;
+            var userManager = serviceProvider.GetRequiredService<UserManager<User>>();
 
-            return Ok();
+            if (!userManager.SupportsUserEmail)
+            {
+                throw new NotSupportedException($"{nameof(Register)} requires a user store with email support.");
+            }
+
+            var userStore = serviceProvider.GetRequiredService<IUserStore<User>>();
+            var emailStore = (IUserEmailStore<User>)userStore;
+            var email = userRegistrationModel.Email;
+
+            if (string.IsNullOrEmpty(email) || !_emailAddressAttribute.IsValid(email))
+            {
+                return CreateValidationProblem(IdentityResult.Failed(userManager.ErrorDescriber.InvalidEmail(email)));
+            }
+
+            var user = new User();
+            await userStore.SetUserNameAsync(user, email, CancellationToken.None);
+            await emailStore.SetEmailAsync(user, email, CancellationToken.None);
+            user.FirstName = userRegistrationModel.FirstName;
+            user.LastName = userRegistrationModel.LastName;
+            
+            var result = await userManager.CreateAsync(user, userRegistrationModel.Password);
+            await userManager.AddToRoleAsync(user, "CruiseManager");
+            
+            if (!result.Succeeded)
+            {
+                return CreateValidationProblem(result);
+            }
+
+            // await SendConfirmationEmailAsync(user, userManager, context, email);
+            return TypedResults.Ok();
+        }
+        
+        [HttpPost("login")]
+        public async Task<Results<Ok<AccessTokenResponse>, EmptyHttpResult, ProblemHttpResult>> Login(
+            [FromBody] UserLoginModel userLoginModel,
+            [FromQuery] bool? useCookies, [FromQuery] bool? useSessionCookies,
+            [FromServices] IServiceProvider serviceProvider)
+        {
+            var signInManager = serviceProvider.GetRequiredService<SignInManager<User>>();
+
+            var useCookieScheme = (useCookies == true) || (useSessionCookies == true);
+            var isPersistent = (useCookies == true) && (useSessionCookies != true);
+            
+            signInManager.AuthenticationScheme = useCookieScheme ?
+                IdentityConstants.ApplicationScheme : IdentityConstants.BearerScheme;
+
+            var result = await signInManager.PasswordSignInAsync(
+                userLoginModel.Email, userLoginModel.Password,isPersistent, lockoutOnFailure: true);
+
+            if (!result.Succeeded)
+            {
+                return TypedResults.Problem(result.ToString(), statusCode: StatusCodes.Status401Unauthorized);
+            }
+
+            // The signInManager already produced the needed response in the form of a cookie or bearer token.
+            return TypedResults.Empty;
+        }
+        
+        
+        private static ValidationProblem CreateValidationProblem(IdentityResult result)
+        {
+            // We expect a single error code and description in the normal case.
+            // This could be golfed with GroupBy and ToDictionary, but perf! :P
+            Debug.Assert(!result.Succeeded);
+            var errorDictionary = new Dictionary<string, string[]>(1);
+
+            foreach (var error in result.Errors)
+            {
+                string[] newDescriptions;
+
+                if (errorDictionary.TryGetValue(error.Code, out var descriptions))
+                {
+                    newDescriptions = new string[descriptions.Length + 1];
+                    Array.Copy(descriptions, newDescriptions, descriptions.Length);
+                    newDescriptions[descriptions.Length] = error.Description;
+                }
+                else
+                {
+                    newDescriptions = [error.Description];
+                }
+
+                errorDictionary[error.Code] = newDescriptions;
+            }
+
+            return TypedResults.ValidationProblem(errorDictionary);
         }
     }
 }
