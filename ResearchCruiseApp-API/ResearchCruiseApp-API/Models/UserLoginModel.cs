eed95cbfa44872e76c2aa40a96220e5b5470d69a
Mateusz Kowalczyk <mateusz.kowalczyyk@sspg.pl>
2024-01-04 21:05:39 +0100

    Added custom registration and login

---
+++ ResearchCruiseApp-API/ResearchCruiseApp-API/Models/UserLoginModel.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ResearchCruiseApp_API.Models;
+
+public class UserLoginModel
+{
+    [Required]
+    public string Email { get; set; } = null!;
+    
+    [Required]
+    public string Password { get; set; } = null!;
+}
