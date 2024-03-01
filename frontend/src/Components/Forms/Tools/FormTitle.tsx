90e2160e9cd0ec6578d26df950642c63109babcd
Mateusz Kowalczyk <git.user@sspg.pl>
2024-03-01 22:54:19 +0100

    Refactored FormTitle.tsx

--- frontend/src/Components/Forms/Tools/FormTitle.tsx
+++ frontend/src/Components/Forms/Tools/FormTitle.tsx
@@ -1,8 +1,15 @@
 import React from "react";
 import {Link} from "react-router-dom";
 
-function FormTitle(props:{title:string, sections:(string | boolean)[][]}){
-    function scrollSmoothTo(elementId) {
+
+type Props = {
+    title: string,
+    sections: (string | boolean)[][]
+}
+
+
+function FormTitle(props: Props){
+    function scrollSmoothTo(elementId){
         var element = document.getElementById(elementId);
         element.scrollIntoView({
             block: 'start',
@@ -11,19 +18,33 @@ function FormTitle(props:{title:string, sections:(string | boolean)[][]}){
     }
 
     return (
-        <div className={" mb-2  bg-light z-0 ps-2 pe-2 "}>
-
-            <h1 className={" d-flex flex-column  text-decoration-underline text-end p-2 "}
-                style={{fontSize: "1.5rem"}}>{props.title}</h1>
-            <h1 className={"d-flex flex-row  flex-wrap d-none d-lg-flex p-2"} style={{fontSize: "1.5rem"}}>
+        <div className="mb-2 bg-light z-0 ps-2 pe-2">
+            <h1 className="d-flex flex-column text-decoration-underline text-end p-2 "
+                style={{fontSize: "1.5rem"}}
+            >
+                {props.title}
+            </h1>
+            <h1 className="d-flex flex-row flex-wrap d-none d-lg-flex p-2"
+                style={{fontSize: "1.5rem"}}
+            >
                 {Object.entries(props.sections).map((value, index) => {
-                    return <Link key={`${index}`}
-                        className={`d-flex flex-nowrap m-1 align-self-center text-decoration-none ${value.at(1) ? "text-success" : "text-danger"}`}
-                        style={{fontSize: "0.9rem"}} onClick={()=>scrollSmoothTo(`${index+1}`)}  to={""}>{index + 1}. {value.at(0)}</Link>
+                    return (
+                        <Link key={`${index}`}
+                                 className=
+                                     {`d-flex flex-nowrap m-1 align-self-center text-decoration-none
+                                     ${value.at(1) ? "text-success" : "text-danger"}`}
+                                 style={{fontSize: "0.9rem"}}
+                                 onClick={() => scrollSmoothTo(`${index+1}`)}
+                                 to={""}
+                        >
+                            {index + 1}. {value.at(0)}
+                        </Link>
+                    )
                 })}
             </h1>
         </div>
     )
 }
+
 
 export default FormTitle
