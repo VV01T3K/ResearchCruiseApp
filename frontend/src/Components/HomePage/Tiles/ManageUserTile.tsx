4c6c4cb688e0b36b5459eda6a248935a57211a93
Mateusz Kowalczyk <git.user@sspg.pl>
2024-03-02 21:06:52 +0100

    Refactored up to TilesMenu.tsx

--- frontend/src/Components/HomePage/Tiles/ManageUserTile.tsx
+++ frontend/src/Components/HomePage/Tiles/ManageUserTile.tsx
@@ -1,14 +1,17 @@
 import React  from 'react';
 import Tile from "./Tile";
 import {Link} from "react-router-dom";
-function ManageUserTile( ){
+
+
+function ManageUserTile(){
     return (
         <Tile>
             <div>
-                <Link to={"/ManageUsers"}> Zarządzanie użytkownikami </Link>
+                <Link to="/ManageUsers"> Zarządzanie użytkownikami </Link>
             </div>
         </Tile>
     )
 }
+
 
 export default ManageUserTile
