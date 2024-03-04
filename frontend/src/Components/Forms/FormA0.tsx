6e07054f26f1823c4c7b2ef1687e53d6c37845eb
mateusz_kowalczyk <mateusz.kowalczyk@sspg.pl>
2024-03-04 13:43:10 +0100

    Refactored FormA0.tsx

--- frontend/src/Components/Forms/FormA0.tsx
+++ frontend/src/Components/Forms/FormA0.tsx
@@ -20,33 +20,39 @@ import submit = Simulate.submit;
 import SpubTask from "./Inputs/SpubTask";
 import Api from "../Tools/Api";
 import {DummyTag} from "../Tools/DummyTag";
-function FormA0(){
 
+
+function FormA0(){
     const [userData, setUserData] = useState(null)
 
-    useEffect(()=>{
-        Api.get('/formA').then(response => setUserData(response.data)).catch(()=>{})
-        console.log(userData)
-        return () => {};
-
-    },[]);
+    useEffect(
+        () => {
+            Api.get('/formA')
+                .then(response => setUserData(response.data))
+                .catch(()=> {})
+            console.log(userData)
+            return () => {};
+        },
+        []
+    );
 
     const defaultValues = {
         managers: null,
-        supplyManagers:null,
+        supplyManagers: null,
         years: null,
         acceptedPeriod: [0,24],
-        optimalPeriod:[0,24],
+        optimalPeriod: [0,24],
         cruiseDays: 0,
         cruiseTime: 0,
-        notes:null,
-        shipUsage:null,
-        diffrentUsage:null,
-
-
+        notes: null,
+        shipUsage: null,
+        diffrentUsage: null,
     }
 
-    const form = useForm({mode: 'onBlur',defaultValues: defaultValues});
+    const form = useForm({
+        mode: 'onBlur',
+        defaultValues: defaultValues
+    });
 
         // {
         // control, trigger,
@@ -62,108 +68,178 @@ function FormA0(){
         "Kierownik":"Kierownik zgłaszanego rejsu",
         "Czas":"Czas trwania zgłaszanego rejsu",
         "Pozwolenia": "Dodatkowe pozwolenia do planowanych podczas rejsu badań",
-        "Rejon":"Rejon prowadzenia badań",
-        "Cel":"Cel Rejsu",
-        "L.Osób":"Przewidywana liczba osób załogi naukowej.",
-        "Zadania":"Zadania do zrealizowania w trakcie rejsu",
-        "Umowy":"Lista umów współpracy.",
-        "Z.Badawcze":"Zespoły badawcze jakie miałyby uczestniczyć w rejsie.",
-        "Publikacje/Prace":"Publikacje i Prace",
-        "Efekty":"Efekty rejsu",
-        "SPUB":"Zadanie SPUB."})
+        "Rejon": "Rejon prowadzenia badań",
+        "Cel": "Cel Rejsu",
+        "L. osób": "Przewidywana liczba osób załogi naukowej",
+        "Zadania": "Zadania do zrealizowania w trakcie rejsu",
+        "Umowy": "Lista umów współpracy",
+        "Z. badawcze": "Zespoły badawcze, jakie miałyby uczestniczyć w rejsie",
+        "Publikacje/prace": "Publikacje i prace",
+        "Efekty": "Efekty rejsu",
+        "SPUB": "Zadania SPUB"
+    })
 
     return (
         <FormTemplate>
-            <FormTitle sections={sections} title={"Formularz A"}/>
+            <FormTitle sections={sections} title={"Formularz A"} />
             <FormWithSections form={form} onSubmit={()=>{}}
                               // onChange={()=>console.log(form.getValues())}
             >
                 <FormSection title={sections.Kierownik}>
-                    <FormCreatableSelect className="col-12 col-md-6 col-xl-3" name={"managers"} label={"Kierownik rejsu"} values={["sss"]}/>
-                    <FormSelect className="col-12 col-md-6 col-xl-3" name={"supplyManagers"} label={"Zastępca"} values={userData?.supplyManagers}/>
-                    <FormSelect className="col-12 col-md-6 col-xl-3" name={"years"} label={"Rok rejsu"} values={userData?.years}/>
+                    <FormCreatableSelect className="col-12 col-md-6 col-xl-3"
+                                         name="managers"
+                                         label="Kierownik rejsu"
+                                         values={["sss"]}
+                    />
+                    <FormSelect className="col-12 col-md-6 col-xl-3"
+                                name="supplyManagers"
+                                label="Zastępca"
+                                values={userData?.supplyManagers}
+                    />
+                    <FormSelect className="col-12 col-md-6 col-xl-3"
+                                name="years"
+                                label="Rok rejsu"
+                                values={userData?.years}
+                    />
                 </FormSection>
+
                 <FormSection title={sections.Czas}>
-                    <MonthSlider className={"col-12 col-md-12 col-xl-6 p-5"}
-                                 name="acceptedPeriod" label={"Dopuszczalny okres, w którym miałby się odbywać rejs:"}/>
-                    <MonthSlider className={"col-12 col-md-12 col-xl-6 p-5"}
-                                 name={"optimalPeriod"}  label={"Optymalny okres, w którym miałby się odbywać rejs"} watch={form.watch("acceptedPeriod")}/>
-                    <NumberInput className={"col-12 col-md-12 col-xl-6"} name={"cruiseDays"} label={"Liczba planowanych dób rejsowych"}
-                                 connectedName={"cruiseTime"} notZero
-                                 newVal={(e)=>24*e}   maxVal={99}
-                               />
-                    <NumberInput className={"col-12 col-md-12 col-xl-6"} notZero
-                                 name={"cruiseTime"}  label={"Liczba planowanych godzin rejsowych"} connectedName={"cruiseDays"}
-                                 newVal={(e)=>Number((e/24).toFixed(2))}  maxVal={99}
-                                 />
-
-                    <TextArea className={"col-12 p-3"} required={false}
-                              label={"Uwagi dotyczące teminu"} name={"notes"}/>
-                    <FormRadio
-                        className={"col-12 col-md-12 col-xl-6 p-3"}
-                        label={"Czy statek na potrzeby badań będzie wykorzystywany:"} name={"shipUsage"}
-                               values={["całą dobę","jedynie w ciągu dnia (max. 8-12h)",
-                                   "jedynie w nocy (max. 8-12h)",
-                                   "8-12h w ciągu doby rejsowej, ale bez znaczenia o jakiej porze albo z założenia o różnych porach",
-                               "w inny sposób"]}/>
-
+                    <MonthSlider className="col-12 col-md-12 col-xl-6 p-5"
+                                 name="acceptedPeriod"
+                                 label="Dopuszczalny okres, w którym miałby się odbywać rejs:"
+                    />
+                    <MonthSlider className="col-12 col-md-12 col-xl-6 p-5"
+                                 name="optimalPeriod"
+                                 label="Optymalny okres, w którym miałby się odbywać rejs"
+                                 watch={form.watch("acceptedPeriod")}
+                    />
+                    <NumberInput className="col-12 col-md-12 col-xl-6"
+                                 name="cruiseDays"
+                                 label="Liczba planowanych dób rejsowych"
+                                 connectedName="cruiseTime"
+                                 notZero
+                                 newVal={(e) => 24*e}
+                                 maxVal={99}
+                    />
+                    <NumberInput className="col-12 col-md-12 col-xl-6"
+                                 notZero
+                                 name="cruiseTime"
+                                 label="Liczba planowanych godzin rejsowych"
+                                 connectedName="cruiseDays"
+                                 newVal={(e) => Number((e/24).toFixed(2))}
+                                 maxVal={99}
+                    />
+                    <TextArea className="col-12 p-3"
+                              required={false}
+                              label="Uwagi dotyczące teminu"
+                              name="notes"
+                    />
+                    <FormRadio className="col-12 col-md-12 col-xl-6 p-3"
+                               label="Statek na potrzeby badań będzie wykorzystywany:"
+                               name="shipUsage"
+                               values={[
+                                   "całą dobę",
+                                   "jedynie w ciągu dnia (maks. 8–12 h)",
+                                   "jedynie w nocy (maks. 8–12 h)",
+                                   "8–12 h w ciągu doby rejsowej, ale bez znaczenia o jakiej porze albo z założenia" +
+                                       "o różnych porach",
+                                   "w inny sposób"
+                               ]}
+                    />
                     {(() => {
-                        if ( form.watch("shipUsage") == "w inny sposób" ) {
+                        if (form.watch("shipUsage") == "w inny sposób" ) {
                             return (
-                                <TextArea className={"col-12 col-md-12 col-xl-6 p-3"}
-                                          label={"Inny sposób użycia"} name={"diffrentUsage"} required={"Podaj sposób użycia"}/>
+                                <TextArea className="col-12 col-md-12 col-xl-6 p-3"
+                                          label="Inny sposób użycia"
+                                          name="diffrentUsage"
+                                          required="Podaj sposób użycia"
+                                />
                             )
-                        } else return <DummyTag required={false}/>
+                        }
+                        else
+                            return <DummyTag required={false} />
                     })()}
-
-
                 </FormSection>
+
                 <FormSection title={sections.Pozwolenia}>
-                    <FormRadio
-                        className={"col-12 col-md-12 col-xl-6 p-3"}
-                        label={"Czy do badań prowadzonych podczas rejsu są potrzebne dodatkowe pozwolenia?:"} name={"permissions"}
-                        values={["tak", "nie"]}/>
+                    <FormRadio className="col-12 col-md-12 col-xl-6 p-3"
+                               label="Czy do badań prowadzonych podczas rejsu są potrzebne dodatkowe pozwolenia?"
+                               name="permissions"
+                               values={["tak", "nie"]}
+                    />
                     {(() => {
                         // @ts-ignore
-                        if ( form.watch("permissions") == "tak" ) {
+                        if (form.watch("permissions") == "tak" ) {
                             return (
-                                <TextArea className={"col-12 col-md-12 col-xl-6 p-3"}
-                                          label={"Jakie?"} name={"additionalPermissions"} required={"Podaj jakie"}
-                                           />
+                                <TextArea className="col-12 col-md-12 col-xl-6 p-3"
+                                          label="Jakie?"
+                                          name="additionalPermissions"
+                                          required="Podaj jakie"
+                                />
                             )
-                        }else return <DummyTag required={false}/>
+                        }
+                        else
+                            return <DummyTag required={false} />
                     })()}
                 </FormSection>
+
                 <FormSection title={sections.Rejon}>
-                   <ClickableMap label={"Obszar prowadzonych badań"} name={"area"}/>
-                    <TextArea className={"col-12 col-md-12 col-xl-6 p-3"} required={false}
-                              label={"Opis"} name={"areaInfo"}/>
+                    <ClickableMap label="Obszar prowadzonych badań" name="area" />
+                    <TextArea className="col-12 col-md-12 col-xl-6 p-3"
+                              required={false}
+                              label="Opis"
+                              name="areaInfo"
+                    />
                 </FormSection>
+
                 <FormSection title={sections.Cel}>
-                    <FormRadio
-                        className={"col-12 col-md-12 col-xl-6 p-3"}
-                        label={"Cel rejsu"} name={"goal"}
-                        values={["Naukowy", "Komercyjny", "Dydaktyczny"]} />
-                    <TextArea className={"col-12 col-md-12 col-xl-6 p-3"}
-                              label={"Opis"} name={"goalaInfo"} required={"Opisz cel"}/>
+                    <FormRadio className="col-12 col-md-12 col-xl-6 p-3"
+                               label="Cel rejsu"
+                               name="goal"
+                               values={["Naukowy", "Komercyjny", "Dydaktyczny"]}
+                    />
+                    <TextArea className="col-12 col-md-12 col-xl-6 p-3"
+                              label="Opis"
+                              name="goalaInfo"
+                              required="Opisz cel"
+                    />
                 </FormSection>
-                <FormSection  title={sections["L.Osób"]}>
-                    <NumberInput   className={"col-12 col-md-12 col-xl-6 p-3"}
-                            label={"Pracownicy UG"} name={"ugEmployees"} maxVal={20}/>
-                    <NumberInput   className={"col-12 col-md-12 col-xl-6 p-3"}
-                                label={"Studenci I, II st. i doktoranci"} name={"students"} maxVal={20}/>
-                    <NumberInput   className={" col-12 col-md-12 col-xl-6 p-3"}
-                                label={"Goście / osoby spoza UG"} name={"guests"}  maxVal={20}/>
+
+                <FormSection  title={sections["L. osób"]}>
+                    <NumberInput className="col-12 col-md-12 col-xl-6 p-3"
+                                 label="Pracownicy UG"
+                                 name="ugEmployees"
+                                 maxVal={20}
+                    />
+                    <NumberInput className="col-12 col-md-12 col-xl-6 p-3"
+                                 label="Studenci I, II st. i doktoranci"
+                                 name="students"
+                                 maxVal={20}
+                    />
+                    <NumberInput className="col-12 col-md-12 col-xl-6 p-3"
+                                 label="Goście / osoby spoza UG"
+                                 name="guests"
+                                 maxVal={20}
+                    />
                 </FormSection>
+
                 <FormSection title={sections.Zadania}>
-                    <NumberInput   className={" col-12 col-md-12 col-xl-6 p-3"}
-                                   label={"Goście / osoby spoza UG"} name={"gusests"}  maxVal={20}/>
+                    <NumberInput className="col-12 col-md-12 col-xl-6 p-3"
+                                 label="Goście / osoby spoza UG"
+                                 name="gusests"
+                                 maxVal={20}
+                    />
                     {/*<TaskInput name={"wejscie"} className={"col-12"} label={""}/>*/}
                 </FormSection>
+
                 <FormSection title={sections.Umowy}>
-                    <NumberInput   className={" col-12 col-md-12 col-xl-6 p-3"}
-                                   label={"Goście / osoby spoza UG"} name={"gusests"}  maxVal={20}/>
+                    <NumberInput className="col-12 col-md-12 col-xl-6 p-3"
+                                 label="Goście / osoby spoza UG"
+                                 name="gusests"
+                                 maxVal={20}
+                    />
                 </FormSection>
+
                 {/*<FormSection title={sections["Z.Badawcze"]}>*/}
                 {/*    /!*<BlockListInput className={"col-12 col-xl-4 "} label={"Uczestnictwo naukowców spoza UG"} name={"blockListInput2"}/>*!/*/}
                 {/*    /!*<BlockListInput className={"col-12 col-xl-4 "} label={"Uczestnictwo naukowców z jednostek organizacyjnych UG spoza WOiG"} name={"blockListInput"}/>*!/*/}
@@ -184,8 +260,8 @@ function FormA0(){
             {/*    <button type={"submit"}/>*/}
             </FormWithSections>
         </FormTemplate>
-
     )
 }
+
 
 export default FormA0
