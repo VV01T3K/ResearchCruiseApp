a8a6d60c68cda11acf1462089634441d307bafe1
Michal Tarnacki <michaltarnacki@icloud.com>
2024-03-09 00:47:57 +0100

    fixed form inputs

--- frontend/src/Components/Forms/Tools/FormTitle.tsx
+++ frontend/src/Components/Forms/Tools/FormTitle.tsx
@@ -2,6 +2,7 @@ import React, {useEffect, useState} from "react";
 import {Link} from "react-router-dom";
 import useCustomEvent from "../../Tools/useCustomEvent";
 import Api from "../../Tools/Api";
+import {keyboard} from "@testing-library/user-event/dist/keyboard";
 
 
 type Props = {
@@ -19,9 +20,12 @@ function FormTitle(props: Props){
         });
     }
 
+
     const [sections, setSections] = useState(
-        Object.keys(props.sections).map((key) => { return {[key]:false};
-        })
+        Object.keys(props.sections).reduce((acc, key) => {
+        acc[key] = false;
+        return acc;
+    }, {})
     )
 
 
@@ -29,25 +33,10 @@ function FormTitle(props: Props){
 
     useEffect(() => {
         const unsubscribeLogin = sectionStateListener((data) => {
-
-                // console.log(Object.keys(sections).map((key) => {
-                //     const value = sections[key];
-                //     if (key==Object.entries(data)[0][0]) {
-                //         return { [key]: Object.entries(data)[0][1] }; // Zamień 'x' na 'y'
-                //     }
-                //     return {key: value};
-                // }))
-    // console.log(Object.entries(data)[0][0])
-            console.log(sections)
-
-                    // const tmpSections = sections
-                    //
-                    // const firstKey = tmpSections[Object.keys(data)]
-                    // tmpSections[Object.keys(data)][Object.keys(firstKey)] = data[Object.keys(data)]
-                    // setSections(sections[data.key] = data.value)
-
-             // console.log(data)
-            }
+           setSections((sections) => ({
+               ...sections,
+               [Object.keys(data)]:Object.values(data)[0]
+           }))}
 
             );
         return () => {
@@ -62,12 +51,10 @@ function FormTitle(props: Props){
             <h1 className={" d-flex flex-column  text-decoration-underline text-end p-2 "}
                 style={{fontSize: "1.5rem"}}>{props.title}</h1>
             <h1 className={"d-flex flex-row  flex-wrap d-none d-lg-flex p-2"} style={{fontSize: "1.5rem"}}>
-                {Object.entries(sections).map((value, index) => {
-                    const section = value.at(1)
-
+                {Object.entries(sections).map(([key,value], index) => {
                     return <Link key={`${index}`}
-                        className={`d-flex flex-nowrap m-1 align-self-center text-decoration-none ${!section[Object.keys(section)] ? "text-success" : "text-danger"}`}
-                        style={{fontSize: "0.9rem"}} onClick={()=>scrollSmoothTo(`${index+1}`)}  to={""}>{index + 1}. {Object.keys(section)}</Link>
+                        className={`d-flex flex-nowrap m-1 align-self-center text-decoration-none ${value ? "text-success" : "text-danger"}`}
+                        style={{fontSize: "0.9rem"}} onClick={()=>scrollSmoothTo(`${index+1}`)}  to={""}>{index + 1}. {key}</Link>
                 })}
             </h1>
         </div>
