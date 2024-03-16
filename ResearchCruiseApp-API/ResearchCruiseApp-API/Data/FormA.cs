b185fafd0989e9e2a32012678ecf5832eb0bcb49
Mateusz Kowalczyk <git.user@sspg.pl>
2024-03-16 20:13:54 +0100

    Reset of ResearchCruiseContext

--- ResearchCruiseApp-API/ResearchCruiseApp-API/Data/FormA.cs
+++ ResearchCruiseApp-API/ResearchCruiseApp-API/Data/FormA.cs
@@ -12,24 +12,24 @@ public class DateRange
 
 public class FormA
 {
-    public int Id { get; set; }
-    public User CruiseManager { get; set; } = null!;  //User
-    public User Deputy { get; set; } = null!; //User
-    public string Year { get; set; } = null!;
-    public DateRange PermissibleDate { get; set; } = null!;
-    public DateRange OptimalDate { get; set; } = null!;
-    public int CruiseHours { get; set; }
-    public string DateComment { get; set; } = null!;
-    public string ShipUsage { get; set; } = null!;
-    public string Permissions { get; set; } = null!;
-    public string ResearchArea { get; set; } = null!;
-    public string CruiseGoal { get; set; } = null!;
-    public int UGWorkers { get; set; }
-    public int Students { get; set; }
-    public int Guests { get; set; }
-    public string OrganizationalUnit { get; set; } = null!;
-    public List<Contract> ContractsList { get; set; } = null!;
-    public List<TaskToDo> ResearchTask { get; set; } = null!;
-    public List<SPUBTask> SPUBTasks { get; set; } = null!;
+    // public int Id { get; set; }
+    // public User CruiseManager { get; set; } = null!;  //User
+    // public User Deputy { get; set; } = null!; //User
+    // public string Year { get; set; } = null!;
+    // public DateRange PermissibleDate { get; set; } = null!;
+    // public DateRange OptimalDate { get; set; } = null!;
+    // public int CruiseHours { get; set; }
+    // public string DateComment { get; set; } = null!;
+    // public string ShipUsage { get; set; } = null!;
+    // public string Permissions { get; set; } = null!;
+    // public string ResearchArea { get; set; } = null!;
+    // public string CruiseGoal { get; set; } = null!;
+    // public int UGWorkers { get; set; }
+    // public int Students { get; set; }
+    // public int Guests { get; set; }
+    // public string OrganizationalUnit { get; set; } = null!;
+    // public List<Contract> ContractsList { get; set; } = null!;
+    // public List<TaskToDo> ResearchTask { get; set; } = null!;
+    // public List<SPUBTask> SPUBTasks { get; set; } = null!;
     
 }
