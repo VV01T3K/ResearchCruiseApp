02f366eaa9945757e317aaeb1bd83cf4af0d1f97
Mateusz Kowalczyk <git.user@sspg.pl>
2024-04-26 21:52:25 +0200

    Fixed non-working FormA entity

--- ResearchCruiseApp-API/ResearchCruiseApp-API/Data/FormA.cs
+++ ResearchCruiseApp-API/ResearchCruiseApp-API/Data/FormA.cs
@@ -11,21 +11,29 @@ namespace ResearchCruiseApp_API.Data;
 public class FormA
 {
     [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
-    public Guid Id { get; set; }
+    public int Id { get; set; }
+    
     public User CruiseManager { get; set; }  //User
+    
     public User Deputy { get; set; } //User
+    
     [Range(2024, 2050)]
     public int Year { get; set; }
+    
     [Length(2,2)]
     [Range(0,24)]
     public HashSet<int> PermissibleDate { get; set; }
+    
     [Length(2,2)]
     [Range(0,24)]
     public HashSet<int> OptimalDate { get; set; }
+    
     [Range(1,99)]
     public int CruiseHours { get; set; }
+    
     [StringLength(200)]
     public string DateComment { get; set; }
+    
     [Range(0,4)]
     public int ShipUsage { get; set; }
     
@@ -42,15 +50,20 @@ public class FormA
             }
         }
     }
+    
     [MaxLength(200)]
     public string? AdditionalPermissions { get; set; }
     /* TODO Zdobyć wymagania co do obszaru */
+    
     [Range(0,20)]
     public int ResearchArea { get; set; } 
+    
     [MaxLength(200)]
     public string CruiseGoal { get; set; }
-    public List<IResearchTask> ResearchTasks { get; set; } 
-    public List<Contract>  Contracts { get; set; } 
+    
+    // public List<IResearchTask> ResearchTasks { get; set; } 
+    
+    public List<Contract> Contracts { get; set; } 
     
     // public string OrganizationalUnit { get; set; } ;
     // public List<Contract> ContractsList { get; set; } ;
