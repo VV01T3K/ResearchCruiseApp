aafda126e1da8972c8ca51ca6817fe06b19250a3
matowaty <matinowak24@gmail.com>
2024-04-26 20:25:51 +0200

    Modified Mapper to auto map both ways from model to DTO and otherwise

--- ResearchCruiseApp-API/ResearchCruiseApp-API/Tools/MapperConfig.cs
+++ ResearchCruiseApp-API/ResearchCruiseApp-API/Tools/MapperConfig.cs
@@ -12,7 +12,27 @@ namespace ResearchCruiseApp_API.Tools
             var config = new MapperConfiguration(cfg =>
             {
                 //Configuring FormsModel and FormA
-                cfg.CreateMap<FormsModel, FormA>();
+                cfg.CreateMap<FormsModel, FormA>()
+                    .ForMember(dest => dest.Year, act => act.MapFrom(src => src.CruiseInfoData.Year))
+                    .ForMember(dest => dest.CruiseHours, act => act.MapFrom(src => src.CruiseInfoData.CruiseHours))
+                    .ForMember(dest => dest.DateComment, act => act.MapFrom(src => src.CruiseInfoData.DateComment))
+                    .ForMember(dest => dest.ShipUsage, act => act.MapFrom(src => src.CruiseInfoData.ShipUsage))
+                    ;// .ReverseMap()
+                    // .ForPath(dest => dest.CruiseInfoData.Year, opt => opt.MapFrom(src => src.Year))
+                    // .ForPath(dest => dest.CruiseInfoData.CruiseHours, opt => opt.MapFrom(src => src.CruiseHours))
+                    // .ForPath(dest => dest.CruiseInfoData.DateComment, opt => opt.MapFrom(src => src.DateComment))
+                    // .ForPath(dest => dest.CruiseInfoData.ShipUsage, opt => opt.MapFrom(src => src.ShipUsage));
+
+                // cfg.CreateMap<FormA, FormsModel>()
+                //     .ForPath(dest => dest.CruiseInfoData.Year, opt => opt.MapFrom(src => src.Year))
+                //     .ForPath(dest => dest.CruiseInfoData.CruiseHours, opt => opt.MapFrom(src => src.CruiseHours))
+                //     .ForPath(dest => dest.CruiseInfoData.DateComment, opt => opt.MapFrom(src => src.DateComment))
+                //     .ForPath(dest => dest.CruiseInfoData.ShipUsage, opt => opt.MapFrom(src => src.ShipUsage));
+
+                cfg.CreateMap<FormA, FormsModel>()
+                    .ForMember(dest => dest.CruiseInfoData,
+                        opt => opt.MapFrom(src => new CruiseInfo() { Year = src.Year, CruiseHours = src.CruiseHours, DateComment = src.DateComment, ShipUsage = src.ShipUsage}));
+
             });
             
             //Create an Instance of Mapper and return that Instance
