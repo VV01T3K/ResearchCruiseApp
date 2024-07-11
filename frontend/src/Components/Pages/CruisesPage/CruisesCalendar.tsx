ed1656784c54aafede4e0529db97519d3f3e27d5
Mateusz Kowalczyk <git.user@sspg.pl>
2024-07-11 21:00:26 +0200

    First try on react-big-calendar

--- frontend/src/Components/Pages/CruisesPage/CruisesCalendar.tsx
+++ frontend/src/Components/Pages/CruisesPage/CruisesCalendar.tsx
@@ -1,7 +1,14 @@
 import {Calendar, momentLocalizer} from "react-big-calendar";
-import React from "react";
+import React, {useEffect, useState} from "react";
 import moment from "moment/moment";
 import {Cruise} from "./CruisesPage";
+
+
+type CalendarCruiseEvent = {
+    start: Date,
+    end: Date,
+    title: string
+}
 
 type Props = {
     cruises?: Cruise[]
@@ -11,10 +18,22 @@ type Props = {
 export default function CruisesCalendar(props: Props) {
     const localizer = momentLocalizer(moment)
 
+    const [cruiseEvents, setCruiseEvents]
+        = useState<CalendarCruiseEvent[] | undefined>()
+    useEffect(() => {
+        const newCruiseEvents: CalendarCruiseEvent[] | undefined = props.cruises?.map(cruise => ({
+            start: new Date(cruise.date.start),
+            end: new Date(cruise.date.end),
+            title: `${cruise.mainCruiseManagerFirstName} ${cruise.mainCruiseManagerLastName}`
+        }))
+        setCruiseEvents(newCruiseEvents)
+        console.log(cruiseEvents)
+    }, []);
+
     return (
         <Calendar
             localizer={localizer}
-            // events={myEventsList}
+            events={cruiseEvents}
             startAccessor="start"
             endAccessor="end"
             style={{ height: 500 }}
