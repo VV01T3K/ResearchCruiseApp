927ef639f8f04354541a6431501c6ecca5f435d3
Michal <michaltarnacki@icloud.com>
2024-10-15 21:06:57 +0200

    shortest commit ever

--- frontend/src/components/Form/Section/SectionExpansionMark.tsx
+++ frontend/src/components/Form/Section/SectionExpansionMark.tsx
@@ -3,6 +3,6 @@ import React from 'react';
 export const ExpansionMark = (props: { isSectionCompleted: boolean, isSectionActive: boolean }) => (
     <div
         className={props.isSectionCompleted ? 'form-section-expansion-mark-default' : 'form-section-expansion-mark-error'}>
-        {props.isSectionActive ? '▲' : '▼'}
+        {props.isSectionActive ? '▼' : '▲'}
     </div>
 );
