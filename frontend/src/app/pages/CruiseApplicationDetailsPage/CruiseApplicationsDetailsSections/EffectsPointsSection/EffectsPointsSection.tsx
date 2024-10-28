59a1bcd5de5c90de7cb07a7c4852bbd2ea4c9161
Mateusz Kowalczyk <116964693+mateusz-kowalczyk-12@users.noreply.github.com>
2024-10-28 22:20:11 +0100

    [RCA-186] Change publications section description (#190)

--- frontend/src/app/pages/CruiseApplicationDetailsPage/CruiseApplicationsDetailsSections/EffectsPointsSection/EffectsPointsSection.tsx
+++ frontend/src/app/pages/CruiseApplicationDetailsPage/CruiseApplicationsDetailsSections/EffectsPointsSection/EffectsPointsSection.tsx
@@ -1,5 +1,4 @@
 import {SectionWrapper} from "@components/Form/Section/SectionWrapper";
-import {effectsSectionFieldNames} from "@app/pages/FormPage/Forms/FormC/FormCSections/EffectsSection";
 import {
     EffectsPointsField
 } from "@app/pages/CruiseApplicationDetailsPage/CruiseApplicationsDetailsSections/EffectsPointsSection/EffectsPointsField";
@@ -8,7 +7,6 @@ export const EffectsPointsSection = () => SectionWrapper(
     {
         shortTitle: 'Efekty',
         longTitle: 'Efekty osiągnięte po poprzednich rejsach',
-        sectionFieldNames: effectsSectionFieldNames,
         children: <EffectsPointsField />
     }
 );
--- frontend/src/app/pages/FormPage/Forms/FormA/FormASections/PublicationsSectionFields.tsx
+++ frontend/src/app/pages/FormPage/Forms/FormA/FormASections/PublicationsSectionFields.tsx
@@ -7,17 +7,21 @@ import { FormAInitValues } from 'FormAInitValues';
 export const PublicationsDescription = () => (
 
     <div className={'p-4 pb-0 text-center'}>
-        <h5 className={'text-center'}>Publikacje związane tematycznie</h5>
-        <p>Publikacje z ubiegłych 5-lat, związane <strong>bezpośrednio </strong>tematycznie z zadaniami
-            do realizacji na planowanym rejsie, <strong>opublikowane przez zespół zaangażowany w
-                realizację rejsu, z afiliacją UG.</strong></p>
-        <h5 className={'text-center'}>Publikacje zawierające dopisek</h5>
-        <p>Publikacje autorstwa zespołu zaangażowanego w realizację rejsu, ALE zawierające dopisek w
+        <h5 className={'text-center'}>Publikacje kategorii <strong>temat</strong></h5>
+        <p>
+            Publikacje z ubiegłych 5 lat związane <strong>bezpośrednio </strong>tematycznie z zadaniami
+            do realizacji na planowanym rejsie <strong>opublikowane przez zespół zaangażowany w
+            realizację rejsu – z afiliacją UG.</strong>
+        </p>
+        <h5 className={'text-center'}>Publikacje kategorii <strong>dopisek</strong></h5>
+        <p>
+            Publikacje autorstwa zespołu zaangażowanego w realizację rejsu, ALE zawierające dopisek w
             treści publikacji (w wersji angielskiej lub w innym języku): <strong>„…the research/study
-                was conducted onboard r/v Oceanograf (the research vessel owned by the University of
-                Gdańsk)…” lub „… samples for the present study were collected during a research cruise
-                onboard r/v Oceanograf…” </strong>lub podobny, ale wskazujący jednoznacznie że badania w
-            ramach niniejszej publikacji były prowadzone z pokładu jednostki RV Oceanograf.</p>
+            was conducted onboard r/v Oceanograf (the research vessel owned by the University of
+            Gdańsk)…”</strong> lub <strong>„… samples for the present study were collected during a research cruise
+            onboard r/v Oceanograf…” </strong>lub podobny, ale wskazujący jednoznacznie, że badania w
+            ramach niniejszej publikacji były prowadzone z pokładu jednostki RV Oceanograf.
+        </p>
     </div>
 );
 
