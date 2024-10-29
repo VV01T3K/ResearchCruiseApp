ce5cb9f693e34eb3ebf96519a000b877c6f76ea3
Michał Tarnacki <michaltarnacki@icloud.com>
2024-10-30 00:43:43 +0100

    fix imports

--- frontend/src/app/pages/FormPage/Forms/FormB/FormBSections.tsx
+++ frontend/src/app/pages/FormPage/Forms/FormB/FormBSections.tsx
@@ -6,7 +6,6 @@ import { ContractSection } from './FormBSections/ContractSection';
 import { ResearchTeamsSection } from './FormBSections/ResearchTeamsSection';
 import { PublicationsSection } from './FormBSections/PublicationsSection';
 import { SpubTasksSection } from './FormBSections/SpubTasksSection';
-import { SupervisorSection } from './FormBSections/SupervisorSection';
 import {CruiseInfoSection} from "@app/pages/FormPage/Forms/FormB/FormBSections/CruiseInfoSection";
 import {CruiseManagersSection} from "@app/pages/FormPage/Forms/FormB/FormBSections/CruiseManagersSection";
 import {CruiseUsageSection} from "@app/pages/FormPage/Forms/FormB/FormBSections/CruiseUsageSection";
@@ -14,6 +13,7 @@ import {CruiseDetailsSection} from "@app/pages/FormPage/Forms/FormB/FormBSection
 import {CruisePlanSection} from "@app/pages/FormPage/Forms/FormB/FormBSections/CruisePlanSection";
 import {EquipementSection} from "@app/pages/FormPage/Forms/FormB/FormBSections/EquipmentSection";
 import {ShipEquipmentSection} from "@app/pages/FormPage/Forms/FormB/FormBSections/ShipEquipmentSection";
+import {SupervisorSection} from "@app/pages/FormPage/Forms/FormA/FormASections/SupervisorSection";
 
 export const FormBSections = () => [
     CruiseInfoSection(),
--- frontend/src/app/pages/FormPage/Forms/FormC/FormC.tsx
+++ frontend/src/app/pages/FormPage/Forms/FormC/FormC.tsx
@@ -1,16 +1,12 @@
 import React, {useEffect, useState} from 'react';
-import { FormWrapper } from '../FormsMisc';
-import { FormType } from '../../../../../ToBeMoved/Pages/CommonComponents/FormTitleWithNavigation';
-import { FormCSections } from './FormCSections';
-import {Cruise} from "Cruise";
 import cruiseApplicationFromLocation from "@hooks/cruiseApplicationFromLocation";
-import {EmptyFunction} from "@consts/EmptyFunction";
 import { CruiseApplicationContext } from '@contexts/CruiseApplicationContext';
 import { CruiseContext } from '@contexts/CruiseContext';
 import { Cruise } from 'Cruise';
 import { getCruiseForCruiseApplication } from '@api/requests';
 import { EmptyFunction } from '@consts/EmptyFunction';
 import FormTemplate from "@app/pages/FormPage/Wrappers/FormTemplate";
+import {FormCSections} from "@app/pages/FormPage/Forms/FormC/FormCSections";
 
 
 function FormC() {
