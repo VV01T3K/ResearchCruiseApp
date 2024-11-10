abd8b9ca9de859b703251d3524b2b6bf48c89b01
Michał Tarnacki <michaltarnacki@icloud.com>
2024-11-10 13:24:45 +0100

    quick refactor

--- frontend/src/app/pages/Account/AccountPageContent.tsx
+++ frontend/src/app/pages/Account/AccountPageContent.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import { ChangePasswordColumn, CredentialsColumn } from '@app/pages';
 
-export const Content = () => (
-    <div className="account-page-data-row mh-100">
-        <CredentialsColumn />
-        <ChangePasswordColumn />
-    </div>
+export const AccountPageContent = () => (
+  <div className='account-page-data-row mh-100'>
+    <CredentialsColumn />
+    <ChangePasswordColumn />
+  </div>
 );
