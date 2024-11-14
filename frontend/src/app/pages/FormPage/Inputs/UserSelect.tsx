07b44558daae6434013d50926d3e9441d43b844f
Michał Tarnacki <michaltarnacki@icloud.com>
2024-11-14 22:12:05 +0100

    bug fixes + code reformat

--- frontend/src/app/pages/FormPage/Inputs/UserSelect.tsx
+++ frontend/src/app/pages/FormPage/Inputs/UserSelect.tsx
@@ -6,47 +6,47 @@ import { FormField } from './FormYearSelect';
 import { FormUser } from 'FormUser';
 
 type Props = FormField & {
-    initValues?: FormUser[];
-    required?: boolean;
-    defaultValue?: string;
+  initValues?: FormUser[];
+  required?: boolean;
+  defaultValue?: string;
 };
 
 function UserSelect(props: Props) {
-    function findLabel(field: FieldValues) {
-        const item = props.initValues?.find((item) => item.id === field.value);
-        if (item) {
-            return item.firstName + ' ' + item.lastName + '\n\r(' + item.email + ')';
-        }
-        return '';
+  function findLabel(field: FieldValues) {
+    const item = props.initValues?.find((item) => item.id === field.value);
+    if (item) {
+      return item.firstName + ' ' + item.lastName + '\n\r(' + item.email + ')';
     }
+    return '';
+  }
 
-    const optionsMapper = props.initValues?.map((value) => ({
-        label: value.firstName + ' ' + value.lastName + '\n\r(' + value.email + ')',
-        value: value.id,
-    }));
+  const optionsMapper = props.initValues?.map((value) => ({
+    label: value.firstName + ' ' + value.lastName + '\n\r(' + value.email + ')',
+    value: value.id,
+  }));
 
-    const render = ({ field }: FieldValues) => {
-        const currentValue = field.value
-            ? { label: findLabel(field), value: field.value }
-            : null;
+  const render = ({ field }: FieldValues) => {
+    const currentValue = field.value
+      ? { label: findLabel(field), value: field.value }
+      : null;
 
-        return (
-            <SelectWrapper
-                fieldName={props.fieldName}
-                value={currentValue}
-                options={optionsMapper}
-            />
-        );
-    };
+    return (
+      <SelectWrapper
+        fieldName={props.fieldName}
+        value={currentValue}
+        options={optionsMapper}
+      />
+    );
+  };
 
-    const fieldProps = {
-        ...props,
-        rules: { required: props.required ?? 'Wybierz jedną z opcji' },
-        render: render,
-        defaultValue: props.defaultValue ?? '',
-    };
+  const fieldProps = {
+    ...props,
+    rules: { required: props.required ?? 'Wybierz jedną z opcji' },
+    render: render,
+    defaultValue: props.defaultValue ?? '',
+  };
 
-    return <FieldWrapper {...fieldProps} />;
+  return <FieldWrapper {...fieldProps} />;
 }
 
 export default UserSelect;
--- frontend/src/app/pages/FormPage/Wrappers/FieldTableWrapper.tsx
+++ frontend/src/app/pages/FormPage/Wrappers/FieldTableWrapper.tsx
@@ -1,139 +1,172 @@
-import React, { CSSProperties, Ref, useContext, useEffect, useRef, useState } from 'react';
+import React, {
+  CSSProperties,
+  Ref,
+  useContext,
+  useEffect,
+  useRef,
+  useState,
+} from 'react';
 import useWindowWidth from '../../../../hooks/useWindowWidth';
 import { CellContext } from '@contexts/CellContext';
-import { AutoSizer, CellMeasurer, CellMeasurerCache, List } from 'react-virtualized';
-
+import {
+  AutoSizer,
+  CellMeasurer,
+  CellMeasurerCache,
+  List,
+} from 'react-virtualized';
 
 // TODO: [Not important] Split to smaller parts
 
-export const FieldTableWrapper = (title: string, colWidths: number[],
-                                  colTitles: string[], cellMapper: () => (() => React.ReactNode)[],
-                                  BottomMenu: React.JSX.Element | null, emptyText: string, content: any,
-                                  reactWindow?: boolean) => {
-    const windowWidth = useWindowWidth();
+export const FieldTableWrapper = (
+  title: string,
+  colWidths: number[],
+  colTitles: string[],
+  cellMapper: () => (() => React.ReactNode)[],
+  BottomMenu: React.JSX.Element | null,
+  emptyText: string,
+  content: any,
+  reactWindow?: boolean
+) => {
+  const windowWidth = useWindowWidth();
 
-    const rootRef = useRef<HTMLDivElement>(null);
+  const rootRef = useRef<HTMLDivElement>(null);
 
-
-    const ColTitle = () => (
-        <div className="table-field-column-title">
-            <b>{title}</b>
+  const ColTitle = () => (
+    <div className='table-field-column-title'>
+      <b>{title}</b>
+    </div>
+  );
+  const MdColTitle = (props: { title: string; colIndex: number }) => (
+    <>
+      {props.title && (
+        <div
+          className='table-field-column-title-md border-end border-white h-100 d-flex'
+          style={cellWidth(props.colIndex)}
+        >
+          <b>{props.title}</b>
         </div>
-    );
-    const MdColTitle = (props: { title: string, colIndex: number }) => (
-        <>
-            {props.title && <div className="table-field-column-title-md border-end border-white h-100 d-flex"
-                                 style={cellWidth(props.colIndex)}>
-                <b>{props.title}</b>
-            </div>}
-            {!props.title && <></>}
-        </>
+      )}
+      {!props.title && <></>}
+    </>
+  );
+  const TableHeader = () => (
+    <div className='table-field-header d-flex flex-row'>
+      {colTitles.map((title, colIndex) => (
+        <MdColTitle key={colIndex} title={title} colIndex={colIndex} />
+      ))}
+      <ColTitle />
+    </div>
+  );
 
-    );
-    const TableHeader = () => (
-        <div className="table-field-header d-flex flex-row">
-            {colTitles.map((title, colIndex) => (
-                <MdColTitle key={colIndex} title={title} colIndex={colIndex} />
-            ))}
-            <ColTitle />
+  const EmptyRow = () => (
+    <div className='table-field-no-content'>{emptyText}</div>
+  );
+
+  const NonEmptyRow = ({
+    index,
+    style,
+    _ref,
+  }: {
+    index: number;
+    style: CSSProperties | undefined;
+    _ref?: Ref<any>;
+  }) => (
+    <div
+      ref={_ref}
+      className={index % 2 ? 'table-field-odd-row' : 'table-field-even-row'}
+      style={style}
+    >
+      {colWidths.map((_, colIndex) => (
+        <Cell key={colIndex} rowIndex={index} colIndex={colIndex} />
+      ))}
+    </div>
+  );
+
+  const cache = new CellMeasurerCache({
+    defaultHeight: 50,
+    fixedWidth: true,
+  });
+  const TableContent = () => (
+    <div className={'overflow-y-scroll'}>
+      {reactWindow && (
+        <AutoSizer disableHeight>
+          {({ width }) => (
+            <List
+              className={'h-100'}
+              width={width}
+              height={Math.min(
+                cache.getHeight(0, 0) * (content.length + 1) ?? 0,
+                window.innerHeight
+              )}
+              rowCount={content.length}
+              rowHeight={cache.rowHeight}
+              deferredMeasurementCache={cache}
+              rowRenderer={({ index, key, parent, style }) => (
+                <CellMeasurer
+                  key={key}
+                  cache={cache}
+                  parent={parent}
+                  columnIndex={0}
+                  rowIndex={index}
+                >
+                  {({ registerChild }) => (
+                    <NonEmptyRow
+                      key={key}
+                      index={index}
+                      style={style}
+                      _ref={registerChild}
+                    />
+                  )}
+                </CellMeasurer>
+              )}
+            />
+          )}
+        </AutoSizer>
+      )}
+      {!reactWindow && (
+        <div
+          className={'flex-grow-1 overflow-scroll-override d-flex flex-column'}
+        >
+          {content.map((_: any, rowIndex: number) => (
+            <NonEmptyRow key={rowIndex} index={rowIndex} style={undefined} />
+          ))}
         </div>
+      )}
+    </div>
+  );
+
+  const CellContent = () => {
+    const cellContext = useContext(CellContext);
+    const RenderComponent = cellMapper()[cellContext!.colIndex];
+    return (
+      <div
+        className='table-field-row-column'
+        style={cellWidth(cellContext!.colIndex)}
+      >
+        {RenderComponent()}
+      </div>
     );
+  };
 
-    const EmptyRow = () => (
-        <div className="table-field-no-content">
-            {emptyText}
-        </div>
-    );
+  const Cell = (props: { colIndex: number; rowIndex: number }) => (
+    <CellContext.Provider
+      value={{ colIndex: props.colIndex, rowIndex: props.rowIndex }}
+    >
+      <CellContent />
+    </CellContext.Provider>
+  );
 
-    const NonEmptyRow = ({ index, style, _ref }: {
-        index: number,
-        style: CSSProperties | undefined,
-        _ref?: Ref<any>
-    }) => (
-        <div ref={_ref} className={index % 2 ? 'table-field-odd-row' : 'table-field-even-row'} style={style}>
-            {colWidths.map((_, colIndex) => (
-                <Cell key={colIndex} rowIndex={index} colIndex={colIndex} />
-            ))}
-        </div>
-    );
+  function cellWidth(colIndex: number) {
+    return { width: windowWidth >= 768 ? `${colWidths[colIndex]}%` : '100%' };
+  }
 
-    const cache = new CellMeasurerCache({
-        defaultHeight: 50,
-        fixedWidth: true,
-    });
-    const TableContent = () => (
-        <>
-            {reactWindow &&
-                // <div ref={rootRef} className={'h-100 '}>
-                <div ref={rootRef} className="vh-100">
-                    <AutoSizer>
-                        {({ height, width }) => (
-                            <List
-                                width={width}
-                                height={height}
-                                rowCount={content.length}
-                                rowHeight={cache.rowHeight}
-                                deferredMeasurementCache={cache}
-                                rowRenderer={({ index, key, parent, style }) => (
-                                    <CellMeasurer
-                                        key={key}
-                                        cache={cache}
-                                        parent={parent}
-                                        columnIndex={0}
-                                        rowIndex={index}
-                                    >
-                                        {({ registerChild }) => (
-                                            <NonEmptyRow key={key} index={index} style={style} _ref={registerChild} />)}
-                                    </CellMeasurer>
-                                )}
-                            />
-                        )
-                        }
-                    </AutoSizer>
-                </div>
-                // </div>
-            }
-            {!reactWindow &&
-                <div className={'flex-grow-1 overflow-scroll-override d-flex flex-column'}>
-                    {content.map((_: any, rowIndex: number) => (
-                        <NonEmptyRow key={rowIndex} index={rowIndex} style={undefined} />
-                    ))}
-                </div>
-            }
-        </>
-
-
-    );
-
-
-    const CellContent = () => {
-        const cellContext = useContext(CellContext);
-        const RenderComponent = cellMapper()[cellContext!.colIndex];
-        return (
-            <div className="table-field-row-column" style={cellWidth(cellContext!.colIndex)}>
-                {RenderComponent()}
-            </div>
-        );
-    };
-
-    const Cell = (props: { colIndex: number, rowIndex: number }) =>
-        (
-            <CellContext.Provider value={{ colIndex: props.colIndex, rowIndex: props.rowIndex }}>
-                <CellContent />
-            </CellContext.Provider>
-        );
-
-    function cellWidth(colIndex: number) {
-        return { width: windowWidth >= 768 ? `${colWidths[colIndex]}%` : '100%' };
-    }
-
-    const Render = (props: { className?: string }) => (
-        <div className={'table-striped d-flex flex-column ' + props.className}>
-            <TableHeader />
-            {!(content?.length > 0) && <EmptyRow />}
-            {content && content.length > 0 && <TableContent />}
-            {BottomMenu}
-        </div>
-    );
-    return { Render };
+  const Render = (props: { className?: string }) => (
+    <div className={'table-striped d-flex flex-column ' + props.className}>
+      <TableHeader />
+      {!(content?.length > 0) && <EmptyRow />}
+      {content && content.length > 0 && <TableContent />}
+      {BottomMenu}
+    </div>
+  );
+  return { Render };
 };
