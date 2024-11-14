c9d5aedd0a39efd5118c9195ffeed5e4fd6cf085
Michał Tarnacki <michaltarnacki@icloud.com>
2024-11-14 22:32:38 +0100

    calendar link fix

--- frontend/src/app/pages/CruisesPage/CruisesCalendar.tsx
+++ frontend/src/app/pages/CruisesPage/CruisesCalendar.tsx
@@ -8,62 +8,62 @@ import { Cruise } from 'Cruise';
 import { EMPTY_GUID } from '@consts/emptyGuid';
 
 type CalendarCruiseEvent = {
-    start: Date;
-    end: Date;
-    title: string;
-    fullCruise: Cruise;
+  start: Date;
+  end: Date;
+  title: string;
+  fullCruise: Cruise;
 };
 
 type Props = {
-    cruises?: Cruise[];
+  cruises?: Cruise[];
 };
 
 export default function CruisesCalendar(props: Props) {
-    const localizer = momentLocalizer(moment);
-    const navigate = useNavigate();
-    const cruisesStateContext = useContext(CruiseStateContext);
+  const localizer = momentLocalizer(moment);
+  const navigate = useNavigate();
+  const cruisesStateContext = useContext(CruiseStateContext);
 
-    const [cruiseEvents, setCruiseEvents] = useState<
-        CalendarCruiseEvent[] | undefined
-    >();
-    useEffect(() => {
-        const newCruiseEvents: CalendarCruiseEvent[] | undefined =
-            cruisesStateContext!.cruises?.map((cruise) => ({
-                start: new Date(cruise.startDate),
-                end: new Date(cruise.endDate),
-                title:
-                    cruise.mainCruiseManagerId != EMPTY_GUID
-                        ? `Kierownik: ${cruise.mainCruiseManagerFirstName} ${cruise.mainCruiseManagerLastName}`
-                        : 'Rejs bez kierownika',
-                fullCruise: cruise,
-            }));
-        setCruiseEvents(newCruiseEvents);
-    }, []);
+  const [cruiseEvents, setCruiseEvents] = useState<
+    CalendarCruiseEvent[] | undefined
+  >();
+  useEffect(() => {
+    const newCruiseEvents: CalendarCruiseEvent[] | undefined =
+      cruisesStateContext!.cruises?.map((cruise) => ({
+        start: new Date(cruise.startDate),
+        end: new Date(cruise.endDate),
+        title:
+          cruise.mainCruiseManagerId != EMPTY_GUID
+            ? `Kierownik: ${cruise.mainCruiseManagerFirstName} ${cruise.mainCruiseManagerLastName}`
+            : 'Rejs bez kierownika',
+        fullCruise: cruise,
+      }));
+    setCruiseEvents(newCruiseEvents);
+  }, []);
 
-    return (
-        <div className={'calendar-container'}>
-            <Calendar
-                localizer={localizer}
-                culture={'pl'}
-                events={cruiseEvents}
-                startAccessor="start"
-                className="calendar"
-                endAccessor="end"
-                views={{ month: true, week: true }}
-                messages={{
-                    month: 'miesiąc',
-                    week: 'tydzień',
-                    day: 'dzień',
-                    today: 'dzisiaj',
-                    previous: '🡸',
-                    next: '🡺',
-                }}
-                onSelectEvent={(e: CalendarCruiseEvent) => {
-                    navigate('/CruiseForm', {
-                        state: { cruise: e.fullCruise },
-                    });
-                }}
-            />
-        </div>
-    );
+  return (
+    <div className={'calendar-container mb-5'}>
+      <Calendar
+        localizer={localizer}
+        culture={'pl'}
+        events={cruiseEvents}
+        startAccessor='start'
+        className='calendar'
+        endAccessor='end'
+        views={{ month: true, week: true }}
+        messages={{
+          month: 'miesiąc',
+          week: 'tydzień',
+          day: 'dzień',
+          today: 'dzisiaj',
+          previous: '🡸',
+          next: '🡺',
+        }}
+        onSelectEvent={(e: CalendarCruiseEvent) => {
+          navigate('/CruiseForm', {
+            state: { cruise: e.fullCruise, readOnly: true },
+          });
+        }}
+      />
+    </div>
+  );
 }
