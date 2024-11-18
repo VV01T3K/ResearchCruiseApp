c77ead5f894d17d30343fc6c07e2d148e5e9c0cf
Michał Tarnacki <michaltarnacki@icloud.com>
2024-11-18 17:06:23 +0100

    fix mypublication routing

--- frontend/src/route/routes/AdministratorRoute.tsx
+++ frontend/src/route/routes/AdministratorRoute.tsx
@@ -8,30 +8,28 @@ import CruiseApplicationDetailsPage from '@app/pages/CruiseApplicationDetailsPag
 import CruisesPage from '@app/pages/CruisesPage/CruisesPage';
 import CruiseFormPage from '@app/pages/CruiseFormPage/CruiseFormPage';
 import HelpInformationPage from '@app/pages/HelpInformationPage/HelpInformationPage';
-import MyPublicationsPage from "@app/pages/MyPublications/MyPublicationsPage";
-import CruiseEffectsPage from "@app/pages/CruiseEffects/CruiseEffectsPage";
+import MyPublicationsPage from '@app/pages/MyPublications/MyPublicationsPage';
+import CruiseEffectsPage from '@app/pages/CruiseEffects/CruiseEffectsPage';
 
 export const AdministratorRoute = () => {
-    return (
-        <>
-            {/*<Route path={Path.SavedApplications} element={<SavedFormPage />} />*/}
-            {/*<Route path={Path.NewForm} element={<NewFormPage />} />*/}
-            <Route path={Path.Form} element={<FormPage />} />
-            <Route path={Path.ManageUsers} element={<ManageUsersPage />} />
-            <Route path={Path.Any} element={<AdminPanel />} />
-            {/*<Route path={Path.Messages} element={<MessagesPage />} />*/}
-            <Route
-                path={Path.CruiseApplications}
-                element={<CruiseApplicationsPage />}
-            />
-            <Route
-                path={Path.CruiseApplicationDetails}
-                element={<CruiseApplicationDetailsPage />}
-            />
-            <Route path={Path.Cruises} element={<CruisesPage />} />
-            <Route path={Path.CruiseForm} element={<CruiseFormPage />} />
-            <Route path={Path.MyPublications} element={<MyPublicationsPage/>}/>
-            <Route path={Path.CruiseEffects} element={<CruiseEffectsPage/>}/>
-        </>
-    );
+  return (
+    <>
+      {/*<Route path={Path.SavedApplications} element={<SavedFormPage />} />*/}
+      {/*<Route path={Path.NewForm} element={<NewFormPage />} />*/}
+      <Route path={Path.Form} element={<FormPage />} />
+      <Route path={Path.ManageUsers} element={<ManageUsersPage />} />
+      <Route path={Path.Any} element={<AdminPanel />} />
+      {/*<Route path={Path.Messages} element={<MessagesPage />} />*/}
+      <Route
+        path={Path.CruiseApplications}
+        element={<CruiseApplicationsPage />}
+      />
+      <Route
+        path={Path.CruiseApplicationDetails}
+        element={<CruiseApplicationDetailsPage />}
+      />
+      <Route path={Path.Cruises} element={<CruisesPage />} />
+      <Route path={Path.CruiseForm} element={<CruiseFormPage />} />
+    </>
+  );
 };
--- frontend/src/route/routes/CommonAccessRoute.tsx
+++ frontend/src/route/routes/CommonAccessRoute.tsx
@@ -4,13 +4,20 @@ import AccountPage from '@app/pages/Account/AccountPage';
 import PriorityInformationPage from '@app/pages/PriorityInformationPage/PriorityInformationPage';
 import HelpInformationPage from '@app/pages/HelpInformationPage/HelpInformationPage';
 import LoginPage from '@app/pages/NotLoggedInPage/LoginPage/LoginPage';
+import MyPublicationsPage from '@app/pages/MyPublications/MyPublicationsPage';
+import CruiseEffectsPage from '@app/pages/CruiseEffects/CruiseEffectsPage';
 
 export const CommonAccessRoute = () => {
-    return (
-        <>
-            <Route path={Path.AccountSettings} element={<AccountPage />} />
-            <Route path={Path.PriorityInformation} element={<PriorityInformationPage />} />
-            <Route path={Path.HelpInformation} element={<HelpInformationPage />} />
-        </>
-    );
+  return (
+    <>
+      <Route path={Path.AccountSettings} element={<AccountPage />} />
+      <Route
+        path={Path.PriorityInformation}
+        element={<PriorityInformationPage />}
+      />
+      <Route path={Path.HelpInformation} element={<HelpInformationPage />} />
+      <Route path={Path.MyPublications} element={<MyPublicationsPage />} />
+      <Route path={Path.CruiseEffects} element={<CruiseEffectsPage />} />
+    </>
+  );
 };
