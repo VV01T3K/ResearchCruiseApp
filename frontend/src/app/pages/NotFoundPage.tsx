8ec581928b6bce78e70062914fd9d2b6c9932e4e
Michał Tarnacki <michaltarnacki@icloud.com>
2024-11-18 13:49:58 +0100

    style fixes

--- frontend/src/app/pages/NotFoundPage.tsx
+++ frontend/src/app/pages/NotFoundPage.tsx
@@ -6,13 +6,18 @@ import LinkWithState from '@components/Navigation/LinkWithState';
 import { Path } from '../../ToBeMoved/Tools/Path';
 
 function NotFoundPage() {
-    return (
-        <Page className={'common-page '}>
-            <PageTitle title={'Szukana strona nie została znaleziona'} />
-            <img className={'m-5 mb-2 rounded'} src={image} />
-            <LinkWithState className={'text-center fs-5'} to={Path.Default} state={{}} label={'Powrót'} />
-        </Page>
-    );
+  return (
+    <Page className={'common-page '}>
+      <PageTitle title={'Szukana strona nie została znaleziona'} />
+      <img className={'m-5 mb-2 rounded'} src={image} />
+      <LinkWithState
+        className={'text-center fs-5 mb-3'}
+        to={Path.Default}
+        state={{}}
+        label={'Powrót'}
+      />
+    </Page>
+  );
 }
 
 export default NotFoundPage;
