4fd93f942987239c4a23aba83467f09e6f36af30
Michał Tarnacki <michaltarnacki@icloud.com>
2024-11-26 23:50:41 +0100

    add new bg and versionInfo

--- frontend/src/ToBeMoved/App.tsx
+++ frontend/src/ToBeMoved/App.tsx
@@ -8,59 +8,61 @@ import PageHeader from './Pages/PageHeader/PageHeader';
 import RoleBasedRouting from '../route/RoleBasedRouting';
 import WaitingPage from '../app/pages/WaitingPage';
 import { BusyContext } from '@contexts/BusyContext';
+import { VersionInfo } from '@components/VersionInfo';
 
 export const UserContext = createContext<null | {
-    userData: UserData | null;
-    setUserData: Dispatch<SetStateAction<UserData | null>>;
+  userData: UserData | null;
+  setUserData: Dispatch<SetStateAction<UserData | null>>;
 }>(null);
 
 export const OpenedWithLocation = () => {
-    const { search } = useLocation();
-    const searchParams = new URLSearchParams(search);
-    return searchParams.get('data') != null;
+  const { search } = useLocation();
+  const searchParams = new URLSearchParams(search);
+  return searchParams.get('data') != null;
 };
 
 const AppContent = () => {
-    const { isBusy } = BusyEvent();
-    const { SetInterceptors } = Interceptors();
-    const OnAppStart = () => {
-        const [isLoaded, setIsLoaded] = useState(false);
-        if (!isLoaded) {
-            setIsLoaded(true);
-            SetInterceptors();
-        }
-    };
+  const { isBusy } = BusyEvent();
+  const { SetInterceptors } = Interceptors();
+  const OnAppStart = () => {
+    const [isLoaded, setIsLoaded] = useState(false);
+    if (!isLoaded) {
+      setIsLoaded(true);
+      SetInterceptors();
+    }
+  };
 
-    OnAppStart();
+  OnAppStart();
 
-    const openedWithLocation = OpenedWithLocation();
-    return (
-        <div className="vh-100">
-            <PageBackground />
-            {!openedWithLocation && <PageHeader />}
-            {/*<div className={isBusy ? 'visually-hidden' : ' h-100'}>*/}
-            <RoleBasedRouting />
-            {/*</div>*/}
-            {/*{isBusy && <></>*/}
-            {/*// <WaitingPage />*/}
-            {/*}*/}
-        </div>
-    );
+  const openedWithLocation = OpenedWithLocation();
+  return (
+    <div className='vh-100'>
+      <PageBackground />
+      {!openedWithLocation && <PageHeader />}
+      {/*<div className={isBusy ? 'visually-hidden' : ' h-100'}>*/}
+      <RoleBasedRouting />
+      {/*</div>*/}
+      {/*{isBusy && <></>*/}
+      {/*// <WaitingPage />*/}
+      {/*}*/}
+    </div>
+  );
 };
 
 function App() {
-    const [userData, setUserData] = useState<UserData | null>(null);
-    const busyMessage = useState<string | null>(null);
-    const [isBusy, _] = busyMessage;
-    return (
-        <BusyContext.Provider value={busyMessage}>
-            <div style={{ cursor: isBusy ? 'progress' : 'default' }}>
-                <UserContext.Provider value={{ userData, setUserData }}>
-                    <AppContent />
-                </UserContext.Provider>
-            </div>
-        </BusyContext.Provider>
-    );
+  const [userData, setUserData] = useState<UserData | null>(null);
+  const busyMessage = useState<string | null>(null);
+  const [isBusy, _] = busyMessage;
+  return (
+    <BusyContext.Provider value={busyMessage}>
+      <div style={{ cursor: isBusy ? 'progress' : 'default' }}>
+        <UserContext.Provider value={{ userData, setUserData }}>
+          <AppContent />
+          <VersionInfo />
+        </UserContext.Provider>
+      </div>
+    </BusyContext.Provider>
+  );
 }
 
 export default App;
---
+++ frontend/src/components/VersionInfo.tsx
@@ -0,0 +1,5 @@
+import { currentVersion } from '@config/currentVersion';
+
+export const VersionInfo = () => (
+  <div className={'version-info'}>v{currentVersion}</div>
+);
