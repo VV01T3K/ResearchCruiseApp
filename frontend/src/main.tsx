6fbbd94fd48f54b88a94d21b05f0d5ebf52cd2ff
VV01T3K <wojteks.access+github@gmail.com>
2025-10-26 17:29:52 +0000

    feat: React Scan for performance monitoring

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,5 +1,10 @@
 import './index.css';
 
+import { scan } from 'react-scan';
+scan({
+  enabled: true,
+});
+
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
