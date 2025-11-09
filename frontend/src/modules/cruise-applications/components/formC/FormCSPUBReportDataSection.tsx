39efb6b761f85c7b65823424c17a46f13991e59f
VV01T3K <wojteks.access+github@gmail.com>
2025-11-09 12:08:49 +0000

    fix: remove required asterisk from additional description and SPUB report data fields in Form C

--- frontend/src/modules/cruise-applications/components/formC/FormCSPUBReportDataSection.tsx
+++ frontend/src/modules/cruise-applications/components/formC/FormCSPUBReportDataSection.tsx
@@ -27,7 +27,6 @@ export function FormCSPUBReportDataSection() {
             className="h-48"
             errors={getErrors(field.state.meta, hasFormBeenSubmitted)}
             placeholder="Wpisz dodatkowe dane do raportu SPUB"
-            showRequiredAsterisk
             disabled={isReadonly}
           />
         )}
