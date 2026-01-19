7b01de666b89692fa9ab3b9400d8c8da44e8bd30
pawel491 <153681511+pawel491@users.noreply.github.com>
2026-01-19 18:52:24 +0100

    fix: generated password always passes validation rules (#279)

--- backend/ResearchCruiseApp/Infrastructure/Services/RandomGenerator.cs
+++ backend/ResearchCruiseApp/Infrastructure/Services/RandomGenerator.cs
@@ -6,7 +6,11 @@ namespace ResearchCruiseApp.Infrastructure.Services;
 internal class RandomGenerator : IRandomGenerator
 {
     private const int SecureCodeBytesSize = 512;
-    private const int SecurePasswordBytesSize = 16;
+    private const int SecurePasswordLength = 24;
+    private const string Upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    private const string Lower = "abcdefghijklmnopqrstuvwxyz";
+    private const string Digits = "0123456789";
+    private const string Special = "!@#$%^&*()-_=+";
 
     public byte[] CreateSecureCodeBytes()
     {
@@ -15,8 +19,22 @@ internal class RandomGenerator : IRandomGenerator
 
     public string CreateSecurePassword()
     {
-        var passwordBytes = GetRandomBytes(SecurePasswordBytesSize);
-        return Convert.ToBase64String(passwordBytes);
+        const string All = Upper + Lower + Digits + Special;
+        Span<char> password = stackalloc char[SecurePasswordLength];
+
+        // ensure characters are taken from each category
+        password[0] = GetRandomChar(Upper);
+        password[1] = GetRandomChar(Lower);
+        password[2] = GetRandomChar(Digits);
+        password[3] = GetRandomChar(Special);
+
+        // fill rest
+        RandomNumberGenerator.GetItems(All.AsSpan(), password[4..]);
+        RandomNumberGenerator.Shuffle(password);
+        return new string(password);
+
+        static char GetRandomChar(string charset) =>
+            charset[RandomNumberGenerator.GetInt32(charset.Length)];
     }
 
     private static byte[] GetRandomBytes(int size)
