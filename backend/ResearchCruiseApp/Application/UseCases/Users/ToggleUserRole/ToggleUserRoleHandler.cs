550760dd88feb255a31cffe844108e9ec7c1aeb5
Filip Pudlak <f.pudlak@wp.pl>
2026-02-15 14:39:22 +0000

    fix: prevent race condition when removing admin

--- backend/ResearchCruiseApp/Application/UseCases/Users/ToggleUserRole/ToggleUserRoleHandler.cs
+++ backend/ResearchCruiseApp/Application/UseCases/Users/ToggleUserRole/ToggleUserRoleHandler.cs
@@ -1,11 +1,12 @@
 using MediatR;
 using ResearchCruiseApp.Application.Common.Constants;
 using ResearchCruiseApp.Application.ExternalServices;
+using ResearchCruiseApp.Application.ExternalServices.Persistence;
 using ResearchCruiseApp.Application.Models.Common.ServiceResult;
 
 namespace ResearchCruiseApp.Application.UseCases.Users.ToggleUserRole;
 
-public class ToggleUserRoleHandler(IIdentityService identityService)
+public class ToggleUserRoleHandler(IIdentityService identityService, IUnitOfWork unitOfWork)
     : IRequestHandler<ToggleUserRoleCommand, Result>
 {
     public async Task<Result> Handle(
@@ -23,9 +24,22 @@ public class ToggleUserRoleHandler(IIdentityService identityService)
             && request.RoleToggleDto.RoleName == RoleName.Administrator
         )
         {
-            var adminCount = await identityService.GetUsersCountInRole(RoleName.Administrator);
-            if (adminCount <= 1)
-                return Error.Conflict("Nie można usunąć roli ostatniego administratora");
+            return await unitOfWork.ExecuteIsolated(
+                async () =>
+                {
+                    var adminCount = await identityService.GetUsersCountInRole(
+                        RoleName.Administrator
+                    );
+                    if (adminCount <= 1)
+                        return Error.Conflict("Nie można usunąć roli ostatniego administratora");
+
+                    return await identityService.RemoveRoleFromUser(
+                        request.UserId,
+                        request.RoleToggleDto.RoleName
+                    );
+                },
+                cancellationToken
+            );
         }
 
         var result = request.RoleToggleDto.AddRole
