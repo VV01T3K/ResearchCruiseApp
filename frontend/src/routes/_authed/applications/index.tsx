a37b0ee94106577fe2a189fce70bb935daad535c
pawel491 <s197977@student.pg.edu.pl>
2026-08-01 10:32:46 +0000

    fix: hide page header while the first applications page is loading

--- frontend/src/routes/_authed/applications/index.tsx
+++ frontend/src/routes/_authed/applications/index.tsx
@@ -261,11 +261,7 @@ function ApplicationsPage() {
   ];
 
   if (applicationsQuery.isPending) {
-    return (
-      <AppLayout title="Zgłoszenia" variant="wide">
-        <AppLoader />
-      </AppLayout>
-    );
+    return <AppLoader />;
   }
 
   return (
